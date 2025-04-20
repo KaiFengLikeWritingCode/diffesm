175401c872f032603af70d28f7702f1af5206471
dkz <18363666985@163.com>
2025-04-20 13:16:36 +0800

    v0

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-accelerate launch --num_processes=3 src/train.py
+accelerate launch --num_processes=4 src/train.py
+# accelerate launch --num_processes=4 --mixed_precision fp16 src/train.py
