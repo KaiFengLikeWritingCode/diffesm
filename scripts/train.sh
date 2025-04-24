bb46f2af643d63e914b66a3eff6718435148e9f7
lanyun-3090*2 <18363666985@163.com>
2025-04-24 19:38:36 +0800

    can_train by 96*96

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-accelerate launch --num_processes=4 src/train.py
+accelerate launch --num_processes=2 src/train.py
 # accelerate launch --num_processes=4 --mixed_precision fp16 src/train.py
